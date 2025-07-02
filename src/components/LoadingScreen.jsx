f819df68b0ef9c808d9a118859195984473f04a3
Aurorn <aurornispro@gmail.com>
2025-07-02 17:17:30 -0400

    Created a Loading Screen to be called into pages to stop a white blank screen

---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const StyledLoadingScreen = styled.div`
+  min-height: 100svh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #b3e283;
+  font-size: 2rem;
+  letter-spacing: 0.1em;
+  background-color:rgb(36, 36, 36);
+`;
+
+const LoadingScreen = () => (
+  <StyledLoadingScreen />
+);
+
+export default LoadingScreen;
