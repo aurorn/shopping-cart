b8a1e6184e1ab27d0fbbdfa2725cba7db9963183
Aurorn <aurornispro@gmail.com>
2025-07-06 17:07:43 -0400

    Multiple styling changes, deleted no longer used Carousel.JSX and Globals.css. Installed the Swiper library to get a better function carousel.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
-import { Suspense, lazy } from "react";
+import { Suspense, lazy, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/Header'
 import Footer from './components/Footer'
+import CartSideBar from './modules/CartSideBar'
+import { theme } from "./Styles/theme";
+import { ThemeProvider } from "styled-components";
 
 const FrontPage = lazy(() => import("./components/FrontPage"));
 const GamePage = lazy(() => import("./components/GamePage"));
 
 function App() {
+  const [cartOpen, setCartOpen] = useState(false);
+
   return (
-    <BrowserRouter>
-      <Header/>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<FrontPage />} />
-          <Route path="/game/:id" element={<GamePage />} />
-        </Routes>
-      </Suspense>
-      <Footer/>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header onCartClick={() => setCartOpen(true)} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<FrontPage />} />
+            <Route path="/game/:id" element={<GamePage />} />
+          </Routes>
+        </Suspense>
+        <CartSideBar open={cartOpen} onClose={() => setCartOpen(false)}>
+          
+        </CartSideBar>
+        <Footer/>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
---
+++ src/Styles/theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+  background: "#121212",
+  primaryFontColor: "#E0E0E0",
+  secondaryTextColor: "#B0B0B0",
+  dividerColor: "#444444",
+  accent: "#00FF85",  
+  accentHover: "#03B55F"
+};
